f360cd86448d2bf46f5c613f86d7cf1763b48447
19130017 <98450194+dtbinh1511@users.noreply.github.com>
2022-09-16 17:40:51 +0700

    add count-down, product

--- src/app/view/layout/default/default.module.ts
+++ src/app/view/layout/default/default.module.ts
@@ -7,6 +7,9 @@ import {HomeComponent} from "../../page/home/home.component";
 import {CarouselComponent} from "../../component/carousel/carousel.component"
 import {ServicesComponent} from "../../component/services/services.component";
 import {CategoriesComponent} from "../../component/categories/categories.component";
+import {FeaturedComponent} from "../../component/featured/featured.component";
+import {ProductItemComponent} from "../../component/product-item/product-item.component";
+import {CountDownComponent} from "../../component/count-down/count-down.component";
 
 
 @NgModule({
@@ -15,7 +18,10 @@ import {CategoriesComponent} from "../../component/categories/categories.compone
     HomeComponent,
     CarouselComponent,
     ServicesComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    FeaturedComponent,
+    ProductItemComponent,
+    CountDownComponent
   ],
   imports: [
     CommonModule,
